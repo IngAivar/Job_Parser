9aafb744407d2e5146377c5ee00377184b348b86
MiniGan067 <MiniGan067@yandex.ru>
2023-04-09 21:19:25 +0300

    sec step

--- classes/classes.py
+++ classes/classes.py
@@ -1,3 +1,4 @@
+import json
 from abc import ABC, abstractmethod
 
 import requests
@@ -19,12 +20,29 @@ class HeadHunterAPI(Engine):
             "page": page,
             "per_page": 100,
         }
-        return requests.get("https://api.hh.ru/vacancies", params=params, headers=headers).json()["items"]
+        return requests.get("https://api.hh.ru/vacancies", params=params).json()["items"]
 
     def get_vacation(self, keyword, count=1000):
-        pages = 10
+        pages = 1
         response = []
-        for page in pages:
-            print(page)
+
+        for page in range(pages):
+            print(f"Парсинг страницы {page + 1}", end=": ")
             values = self.get_request(keyword, page)
-            pprint(values)
+            print(f"Найдено {len(values)} вакансий.")
+            response.extend(values)
+
+        return response
+
+
+class JSONSaver:
+    def __init__(self, keyword):
+        self.__filename = f"{keyword.title()}.json"
+
+    @property
+    def filename(self):
+        return self.__filename
+
+    def add_vacancies(self, data):
+        with open(self.__filename, 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-from classes.classes import HeadHunterAPI
+from classes.classes import HeadHunterAPI, JSONSaver
 
 
 def main():
+
+    keyword = input("Введите ключевое слово по которому будет искаться вакансия: ")
+    keyword = "python"
+
     # Создание экземпляра класса для работы с API сайтов с вакансиями
     hh_api = HeadHunterAPI()
     # superjob_api = SuperJobAPI()
 
     # Получение вакансий с разных платформ
-    hh_vacancies = hh_api.get_vacancies("Python")
-    # superjob_vacancies = superjob_api.get_vacancies("Python")
+    hh_vacancies = hh_api.get_vacation(keyword)
 
+    # Сохранение информации о вакансиях в файл
+    json_saver = JSONSaver(keyword)
+    json_saver.add_vacancies(hh_vacancies)
+    # json_saver.get_vacancies_by_salary("100 000-150 000 руб.")
+    # json_saver.delete_vacancy(vacancy)
+
+    # superjob_vacancies = superjob_api.get_vacancies("Python")
+    #
     # Создание экземпляра класса для работы с вакансиями
     # vacancy = Vacancy("Python Developer", "<https://hh.ru/vacancy/123456>", "100 000-150 000 руб.",
     #                   "Требования: опыт работы от 3 лет...")
-
-    # Сохранение информации о вакансиях в файл
-    # json_saver = JSONSaver()
-    # json_saver.add_vacancy(vacancy)
-    # json_saver.get_vacancies_by_salary("100 000-150 000 руб.")
-    # json_saver.delete_vacancy(vacancy)
 
     # Функция для взаимодействия с пользователем
     # def user_interaction():
